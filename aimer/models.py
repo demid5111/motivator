391a2800d3a987a404a1ddcb66e27b45e66f73ae
demid5111 <monadv@yandex.ru>
2014-10-20 19:18:08 +0400

    First commit. Models added, not tested

---
+++ aimer/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+class Aim (models.Model):
+  user_id = models.ForeignKey('User', db_column='user_id')
+  time_begin = models.DateTimeField(auto_now_add=True, blank=True)
+  time_end = models.DateTimeField(auto_now_add=True, blank=True)
+  aim_bet = models.IntegerField(db_column='aim_bet')
+  aim_name = models.CharField(max_length=450,db_column='aim_name')
+  aim_description = models.CharField(max_length=450)
+  type_id =  models.ForeignKey('AimType', db_column='type_id')
+  status_id = models.ForeignKey('AimStatus', db_column = 'status_id')
+  class Meta:
+    managed = True
+
+class AimType (model.Model):
+  type_id = models.IntegerField(primary_key=True, db_column='type_id')
+  type_name = models.CharField(max_length=45)
+  class Meta:
+    managed = True
+
+class AimStatus (model.Model):
+  status_id = models.IntegerField(primary_key=True, db_column='status_id')
+  status_name = models.CharField(max_length=45)
+  class Meta:
+    managed = True
